db9462d313739b03a93021c46b85ed682404492d
Joe Petrakovich <sora@macbook-air-sora.local>
2025-12-14 13:14:22 +0900

    redo config to diagnose indent issue

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,87 +14,45 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+
+vim.keymap.set('n', '<leader>pv', ':Ex<CR>')
+
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = "a"
-vim.o.showmode = false
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+vim.o.wrap = false
+
 vim.schedule(function()
-	vim.o.clipboard = "unnamedplus"
+  vim.o.clipboard = 'unnamedplus'
 end)
 
--- Set Tab, Shift, and Indent widths to 4 spaces by default
-vim.opt.tabstop = 2         -- Number of spaces a <Tab> counts for
-vim.opt.shiftwidth = 2     -- Number of spaces to use for auto-indenting
-vim.opt.softtabstop = 2  -- Number of spaces a <Tab> key press inserts/deletes
-
--- Use spaces instead of tabs for indentation
-vim.opt.expandtab = true
-
--- Copy indent from current line when starting a new line
-vim.opt.autoindent = true
-
--- Insert C-style automatic indentation for { } blocks, etc.
--- Note: This is often overridden by the filetype-specific
--- 'indentexpr' enabled by 'filetype indent on'.
-vim.opt.smartindent = true
--- Enable break indent
 vim.o.breakindent = true
-
--- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.o.signcolumn = "yes"
-
--- Decrease update time
+vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = "split"
-
--- Show which line your cursor is on
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.inccommand = 'split'
 vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
+vim.o.colorcolumn = "100"
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = -1
 
--- Setup lazy.nvim
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
 require("lazy").setup({
   spec = {
     { import = "plugins" },
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  { 
-    "catppuccin/nvim", 
-     name = "catppuccin", 
-     lazy = false, 
-     priority = 1000,
-     config = function()
-       vim.cmd([[colorscheme catppuccin]])
-     end,
-  },
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin]])
+    end
 }
-
