d04bc61a7477f4eafb9e8421fb195919a5a4873b
Joe Petrakovich <jpetrakovich@gmail.com>
2025-12-21 21:53:26 +0900

    hide folding column and added a way to add newlines above and below without going into insert mode

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -16,8 +16,13 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>pv', ':Ex<CR>')
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set('n', '<leader>pv', '<cmd>Ex<CR>')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>n', '<cmd>bnext<CR>')
+vim.keymap.set('n', '<leader>p', '<cmd>bprev<CR>')
+vim.keymap.set('n', '<leader>o', 'o<ESC>k') --add a space but stay put in normal mode
+vim.keymap.set('n', '<leader>O', 'O<ESC>j')
+
 
 vim.o.number = true
 vim.o.relativenumber = true
@@ -45,6 +50,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = -1
+vim.o.swapfile = false
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
@@ -55,35 +61,48 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.api.nvim_create_autocmd(
-	"LspAttach",
-	{ --  Use LspAttach autocommand to only map the following keys after the language server attaches to the current buffer
-		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-		callback = function(ev)
-			vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
+  "LspAttach",
+  { --  Use LspAttach autocommand to only map the following keys after the language server attaches to the current buffer
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+      vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
 
-			-- Buffer local mappings.
-			-- See `:help vim.lsp.*` for documentation on any of the below functions
-			local opts = { buffer = ev.buf }
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "<leader>w", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      -- Buffer local mappings.
+      -- See `:help vim.lsp.*` for documentation on any of the below functions
+      local opts = { buffer = ev.buf }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "<leader>w", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-			vim.keymap.set("n", "<leader>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, opts)
+      vim.keymap.set("n", "<leader>f", function()
+        vim.lsp.buf.format({ async = true })
+      end, opts)
 
-			-- Open the diagnostic under the cursor in a float window
-			vim.keymap.set("n", "<leader>d", function()
-				vim.diagnostic.open_float({
-					border = "rounded",
-				})
-			end, opts)
-		end,
-	}
+      vim.keymap.set("v", "<leader>f", function()
+        local start_pos = vim.api.nvim_buf_get_mark(0, "<")
+        local end_pos = vim.api.nvim_buf_get_mark(0, ">")
+
+        vim.lsp.buf.format({
+          async = true,
+          range = {
+            ["start"] = start_pos,
+            ["end"] = end_pos,
+          },
+        })
+      end, { desc = "Format selection with LSP" })
+
+      -- Open the diagnostic under the cursor in a float window
+      vim.keymap.set("n", "<leader>d", function()
+        vim.diagnostic.open_float({
+          border = "rounded",
+        })
+      end, opts)
+    end,
+  }
 )
 
 require("lazy").setup({
